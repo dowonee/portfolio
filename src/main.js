12ed6d1b1a9ea1ddd898330389ef994eabc98a63
정도원 <83629099+dowonee@users.noreply.github.com>
2023-09-10 20:14:24 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -8,3 +8,10 @@ document.addEventListener('scroll', () => {
     header.classList.remove('header--dark');
   }
 });
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
