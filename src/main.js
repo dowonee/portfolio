7ad7ddbaaa456dbf3e423359d44a0680c213c1af
정도원 <83629099+dowonee@users.noreply.github.com>
2023-09-15 01:53:25 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -25,17 +25,14 @@ document.addEventListener('scroll', () => {
   }
 });
 
-const toggleBar = document.querySelector('.header__toggle');
-const headerMenu = document.querySelector('.header__menu');
-const headerShow = () => {
-  toggleBar.style.opacity = 0;
-  headerMenu.style.opacity = 1;
-  console.log(alert);
-};
-const headerHide = () => {
-  toggleBar.style.opacity = 1;
-  headerMenu.style.opacity = 0;
-  console.log(alert);
-};
-toggleBar.addEventListener('click', headerShow);
-toggleBar.addEventListener('click', headerHide);
+// Navbar 토글버트 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
