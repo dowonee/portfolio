05b802a5ad4b01b82b6aa0db01474a04da7f717b
정도원 <83629099+dowonee@users.noreply.github.com>
2023-09-14 21:05:50 +0900

    HTML-Toggle

--- src/main.js
+++ src/main.js
@@ -15,3 +15,27 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+const arrowup = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight * 0.5) {
+    arrowup.style.opacity = 1;
+  } else {
+    arrowup.style.opacity = 0;
+  }
+});
+
+const toggleBar = document.querySelector('.header__toggle');
+const headerMenu = document.querySelector('.header__menu');
+const headerShow = () => {
+  toggleBar.style.opacity = 0;
+  headerMenu.style.opacity = 1;
+  console.log(alert);
+};
+const headerHide = () => {
+  toggleBar.style.opacity = 1;
+  headerMenu.style.opacity = 0;
+  console.log(alert);
+};
+toggleBar.addEventListener('click', headerShow);
+toggleBar.addEventListener('click', headerHide);
