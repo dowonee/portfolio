343c3bc3e737daf7629ea243070cdb078e89ff51
정도원 <83629099+dowonee@users.noreply.github.com>
2023-09-22 12:29:00 +0900

    JS-Projects

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+const category = document.querySelectorAll('.category');
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('projects');
+
+// 카테고리 필터
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === '*' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
