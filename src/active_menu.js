3763ec0324516821d8dfda47fdf92b756bc806ab
정도원 <83629099+dowonee@users.noreply.github.com>
2023-09-28 12:31:55 +0900

    Observer-Part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -19,8 +19,12 @@ const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -29,12 +33,10 @@ function observerCallback(entries) {
   entries.forEach((entry) => {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
-    console.log(entry.target.id);
-    console.log(index);
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
   console.log(visibleSections);
   console.log('무조건 라스트 섹션!', selectLastOne);
@@ -43,9 +45,18 @@ function observerCallback(entries) {
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
   console.log(sectionIds[navIndex]);
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
 }
